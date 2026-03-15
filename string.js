ebfbef2dacbb88ba30d229d29cbabe0338b88bcf
brijnesh32 <brijnesh31@gmail.com>
2026-03-15 22:02:04 +0530

    starting string dsa

---
+++ string.js
@@ -0,0 +1,4 @@
+const string = new String("Hello, World!");
+const string2 = "Hello, World!"; // double quotes
+const string3 = "Hello, World!"; // single quotes
+const string4 = `Hello, World!`; // backticks
